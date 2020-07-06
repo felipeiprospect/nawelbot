47c5954113192fe3d244f6621e67dd1ae77777e0
felipeiprospect <67926828+felipeiprospect@users.noreply.github.com>
2020-07-06 16:09:31 -0400

    Update 1capturadepantalla.js

--- 1capturadepantalla.js
+++ 1capturadepantalla.js
@@ -1,25 +1,39 @@
+// Inicializamos las librerias
 const puppeteer = require('puppeteer');
+
+
+// Representa una función asíncrona que ejecuta el código contenido dentro de la función.
 (async () => {
+
+
+  //Se crea un navegador cuando Puppeteer se conecta a una instancia
   const browser = await puppeteer.launch({
+
+    //Los argumentos los vamos a revisar en más detalle en la sesión 2
     args: ['--no-sandbox', '--disable-setuid-sandbox', "--disable-gpu", '--start-maximized'],
     ignoreHTTPSErrors: true,
-    headless: false,
-    slowMo: 100
+    //false inicia con navegador - true inicia sin navegador
+    headless: false
   });
 
+  // Genera una nueva pestaña dentro del Navegador
   const page = await browser.newPage();
+
+  // Abre una url en el navegador
   await page.goto('https://www.iprospect.com/es/cl/about-us/our-team/');
 
+  // Tamaño de la ventana
   await page.setViewport({
     width: 1280,
     height: 800
   })
 
+  //Función que ejecuta una captura de pantalla
   await page.screenshot({
     path: 'google.png',
     fullPage: true
   });
 
-
+  //Cierra el navegador
   browser.close();
 })();
